745af1e59c3ecb8e6224c86d0ef3b19a091191a1
Mike Rapadas <mike@mrap.me>
2021-01-25 12:33:00 -0800

    Bump weekly goal to Week of January 25th, 2021

--- weekly_plan.py
+++ weekly_plan.py
@@ -33,7 +33,7 @@ def next_week():
 def generate_template():
     week = next_week()
     template = """
-{{{{[[embed]]: ((((cigHIaV2H))))}}}}
+{{{{[[embed]]: ((((hhmYVllMU))))}}}}
 {{{{[[query]]: {{and: [[Daily Design]] {today} }}}}}}
 ## [[DoingToday]]
     Daily Practice
