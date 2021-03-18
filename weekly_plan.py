20eb472e9631ae36d319466ac13898794aa69d97
Mike Rapadas <mike@mrap.me>
2021-03-18 06:21:08 -0700

    Update weekly focus ref

--- weekly_plan.py
+++ weekly_plan.py
@@ -33,7 +33,7 @@ def next_week():
 def generate_template():
     week = next_week()
     template = """
-{{{{[[embed]]: ((((hhmYVllMU))))}}}}
+{{{{[[embed]]: ((((EogZn6tUJ))))}}}}
 {{{{[[query]]: {{and: [[Daily Design]] {today} }}}}}}
 ## [[DoingToday]]
     Daily Practice
