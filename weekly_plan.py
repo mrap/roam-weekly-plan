fbba0fae60beaf992b8a8fc6d852ea0ff2caa3cf
Mike Rapadas <mikerapadas@google.com>
2021-05-31 20:12:09 -0700

    Bumps weekly goals

--- weekly_plan.py
+++ weekly_plan.py
@@ -33,7 +33,7 @@ def next_week():
 def generate_template():
     week = next_week()
     template = """
-{{{{[[embed]]: ((((G6miErNZ0))))}}}}
+{{{{[[embed]]: ((((aVAwvrFjg))))}}}}
 {{{{[[query]]: {{and: [[Daily Design]] {today} }}}}}}
 ## [[DoingToday]]
     Daily Practice
